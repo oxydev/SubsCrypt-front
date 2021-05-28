1aaa888f6ace41910f0deca1dc61e845f6449b1e
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-05-29 00:45:54 +0430

    - Authentcation bug in refreshing the pages has been solved.

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,8 @@ export default function Home() {
   const router = useRouter();
   const { globalState, dispatch } = useContext(UserContext);
   const { auth } = useContext(authContext);
-  const userName = Cookies.get("subscrypt");
-  const password = Cookies.get("subscryptPass");
-  const userType = Cookies.get("SubscryptType");
 
   useEffect(() => {
-    if (auth && !globalState.user) {
-      dispatch({
-        type: "LOAD_USER",
-        payload: { username: userName, password: password, type: userType },
-      });
-    }
     if (userType == "provider") {
       router.push("/provider/");
     } else {
