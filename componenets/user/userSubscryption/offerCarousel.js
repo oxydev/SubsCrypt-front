803881207cff636df3f066341bcfec7ba818572a
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-05-29 21:39:15 +0430

    - Key props warning for offer carousel has been fixed.

--- componenets/user/userSubscryption/offerCarousel.js
+++ componenets/user/userSubscryption/offerCarousel.js
@@ -3,7 +3,7 @@ import OfferCard from "./offerCard";
 import data from "../../../data/offers.json";
 
 export default function OfferCarousel() {
-  const offerCards = data.offers.map((item) => <OfferCard offer={item} />);
+  const offerCards = data.offers.map((item) => <OfferCard key={item.name} offer={item} />);
   return (
     <div className="OfferCarousel">
       <div className="OfferCarousel-itemList">{offerCards}</div>
