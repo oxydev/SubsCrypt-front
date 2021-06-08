01c181e818d6d5aa3f479309680a9a6c5c08ec69
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-06-08 12:37:47 +0430

    - Add check authentication by cookies funxtion to the data functions file.

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,18 +1,12 @@
 import React, { useEffect, useContext } from "react";
 import { useRouter } from "next/router";
 import { UserContext } from "../../context/store";
-import { authContext } from "../_app";
 import { loadUserData } from "../../dataFunctions/getData";
 import SubscryptionOffers from "../../componenets/user/userSubscryption/subscryptionOffers";
 import UserPlansList from "../../componenets/user/userSubscryption/userPlansList";
 
 export default function UserHome() {
-  const router = useRouter();
   const { globalState, dispatch } = useContext(UserContext);
-
-  const user = globalState.user;
-
-  const plans = globalState.plans;
 
   return (
     <div className="userDashboard">
