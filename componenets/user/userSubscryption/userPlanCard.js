7964218621171fa15eb27befa546aecabc72a6de
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-06-15 14:02:50 +0430

    - Subscription process for user plan card has been updated.

--- componenets/user/userSubscryption/userPlanCard.js
+++ componenets/user/userSubscryption/userPlanCard.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import localData from "../../../data/sunscryptionPlans.json";
 import * as utils from "../../../utilities/utilityFunctions";
 import {
@@ -9,18 +9,39 @@ import {
 } from "../../../dataFunctions/publicDataFunctions";
 import { UserContext } from "../../../context/store";
 import { authContext } from "../../../pages/_app";
+import { modalContext } from "../../../context/modal";
 import data from "../../../data/testData/providerAddress.json";
+import SubscriptionModal from "./subscriptionModal";
+
+const subscrypt = import("@oxydev/subscrypt");
 
 export default function UserPlanCard(props) {
-  const plan = props.plan.plan;
+  let plan = props.plan.plan;
   const index = props.index;
   const localPlans = localData.userPlans[index];
   const { globalState, dispatch } = useContext(UserContext);
   const { setAuth } = useContext(authContext);
+  const { modal, setModal } = useContext(modalContext);
   const status = props.plan.status;
-
+  const walletAddress = globalState.user.userWallet;
   const providerAddress = data.providerAddress;
-  // console.log(globalState.wallets[1])
+  const [localLoading, setLocalLoading] = useState(false);
+
+  //Get plan charactrisics
+  async function getCharacs() {
+    console.log(props.plan.providerAddress, props.plan.planIndex);
+    await (await subscrypt)
+      .getPlanCharacteristics(props.plan.provider, props.plan.plan_index)
+      .then((result) => {
+        console.log(result);
+        if (result.status == "Fetched") {
+          plan.characteristics = result.result;
+          let modalElement = <SubscriptionModal plan={plan} handleSubmit={handelModalSubmit} />;
+          setModal(modalElement);
+          setLocalLoading(false);
+        }
+      });
+  }
 
   async function handleRefund() {
     await connectToWallet([], "user", dispatch, setAuth);
@@ -33,6 +54,29 @@ export default function UserPlanCard(props) {
     );
   }
 
+  //Submit function for Subscription modal
+  function handelModalSubmit(e, formData) {
+    e.preventDefault();
+    setModal(null);
+    console.log(formData);
+  }
+
+  //Subscription function
+  function handleSubscribe() {
+    setLocalLoading(true);
+    console.log(walletAddress);
+    getCharacs();
+    // password
+    // username
+    // subscribePlan(
+    //   walletAddress.address,
+    //   getWalletInjector(walletAddress),
+    //   callback,
+    //   providerAddress,
+    //   planIndex
+    // );
+  }
+
   async function handleRenew() {
     await connectToWallet([], "user", dispatch, setAuth);
     renewPlan(
@@ -43,10 +87,6 @@ export default function UserPlanCard(props) {
       0,
       ["value1"]
     );
-  }
-
-  async function handleSubscribe() {
-    console.log("subscribe");
   }
 
   function callback({ events = [], status }) {
@@ -129,7 +169,10 @@ export default function UserPlanCard(props) {
           </div>
           {status == -1 ? (
             <>
-              <button className="UserPlan-subscribeBtn" onClick={handleSubscribe}>
+              <button
+                className={localLoading ? "UserPlan-subscribeBtn loading" : "UserPlan-subscribeBtn"}
+                onClick={handleSubscribe}
+              >
                 Subscribe
               </button>
             </>
