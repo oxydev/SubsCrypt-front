af1fc6ec7af48122d82aca1d61006d1acdcabba5
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-06-16 15:12:36 +0430

    - Modal closing bug has been fixed.

--- context/modal.js
+++ context/modal.js
@@ -9,8 +9,21 @@ export const Modal = (props) => {
     <modalContext.Provider value={{ modal, setModal }}>
       <>
         {props.children}
-        <div className={modal ? "ModalContainer" : "ModalContainer hidden"}>{modal}</div>
-        <div className={modal ? "Filler" : "Filler hidden"}></div>
+        <div className={modal ? "ModalContainer" : "ModalContainer hidden"}>
+          {modal}
+          <span
+            className="Modal-closeBtn"
+            onClick={() => {
+              setModal(null);
+            }}
+          ></span>
+        </div>
+        <div
+          className={modal ? "Filler" : "Filler hidden"}
+          onClick={(e) => {
+            setModal(null);
+          }}
+        ></div>
       </>
     </modalContext.Provider>
   );
