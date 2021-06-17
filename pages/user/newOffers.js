50c1352581911ace0b670bdc95d06f8954e3d46c
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-06-17 15:41:43 +0430

    - New offer page created and it's link added to the sidebar.

---
+++ pages/user/newOffers.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SubscryptionOffers from "../../componenets/user/userSubscryption/subscryptionOffers";
+
+export default function NewOffers(props) {
+  return (
+    <div className="userDashboard">
+      <h1>We have new plans for offering to you</h1>
+      <div className="row">
+        <div className="Container--medium">
+          <SubscryptionOffers />
+        </div>
+        <div className="Container--small"></div>
+      </div>
+    </div>
+  );
+}
