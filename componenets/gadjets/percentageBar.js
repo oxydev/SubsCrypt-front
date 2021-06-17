e8ef820592635b4aea75a1a8e5c3dc2a24d63cbb
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-06-17 23:55:56 +0430

    - Percentage number has been removed from the bar chart.

--- componenets/gadjets/percentageBar.js
+++ componenets/gadjets/percentageBar.js
@@ -4,9 +4,7 @@ export default function PercentageBar(props) {
   const percentage = props.percentage;
   return (
     <div className="PercentageBar">
-      <div className="percentage" style={{ right: `${100 - percentage}%` }}>
-        <span className="PercentNum">{percentage}</span>
-      </div>
+      <div className="percentage" style={{ right: `${100 - percentage}%` }}></div>
     </div>
   );
 }
