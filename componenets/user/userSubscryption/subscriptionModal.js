e1855addf4bcdaebac575461a7bb5aa72f8d8678
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2021-06-18 20:02:02 +0430

    Update subscriptionModal.js

--- componenets/user/userSubscryption/subscriptionModal.js
+++ componenets/user/userSubscryption/subscriptionModal.js
@@ -1,45 +1,51 @@
-import React, { useContext, useState } from "react";
-import { UserContext } from "../../../context/store";
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../../../context/store'
 
-export default function SubscriptionModal(props) {
-  const [formData, setFormData] = useState({});
-  const { globalState, dispatch } = useContext(UserContext);
-  const username = globalState.user.username;
+export default function SubscriptionModal (props) {
+  const [formData, setFormData] = useState({})
+  const { globalState, dispatch } = useContext(UserContext)
+  const username = globalState.user.username
 
-  const { plan, handleSubmit, renew } = props;
-  const characteristics = plan.characteristics;
+  const { plan, handleSubmit, renew } = props
+  console.log(plan)
+  const characteristics = plan.characteristics
   const characsInputs = characteristics.map((item) => (
     <div key={item} className="Modal-InputPart">
       <label>{item}</label>
       <input
         type="text"
         onChange={(e) => {
-          setFormData({ ...formData, [item]: e.target.value });
+          setFormData({ ...formData, [item]: e.target.value })
         }}
       />
     </div>
-  ));
-
+  ))
+  console.log(username)
+  console.log(renew)
+  if (username && !formData.username) {
+    console.log('heey')
+    setFormData({ ...formData, username: username })
+  }
   return (
     <section className="SubscriptionModal">
       <h1>Complete the subscription form</h1>
       <form
         onSubmit={(e) => {
-          handleSubmit(e, formData);
+          handleSubmit(e, formData)
         }}
       >
-        {!username ||
-          !renew(
-            <div className="Modal-InputPart">
-              <label>username:</label>
-              <input
-                type="text"
-                onChange={(e) => {
-                  setFormData({ ...formData, username: e.target.value });
-                }}
-              />
-            </div>
-          )}
+        {(!username &&
+          !renew) && (
+          <div className="Modal-InputPart">
+            <label>username:</label>
+            <input
+              type="text"
+              onChange={(e) => {
+                setFormData({ ...formData, username: e.target.value })
+              }}
+            />
+          </div>
+        )}
 
         {!renew && (
           <div className="Modal-InputPart">
@@ -47,7 +53,7 @@ export default function SubscriptionModal(props) {
             <input
               type="password"
               onChange={(e) => {
-                setFormData({ ...formData, password: e.target.value });
+                setFormData({ ...formData, password: e.target.value })
               }}
             />
           </div>
@@ -55,8 +61,8 @@ export default function SubscriptionModal(props) {
 
         {characsInputs}
 
-        <input className="Modal-SubmitBtn" type="submit" value="Subscribe" />
+        <input className="Modal-SubmitBtn" type="submit" value="Subscribe"/>
       </form>
     </section>
-  );
+  )
 }
