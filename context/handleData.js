db65bde4227d0c5b83c16936432dea406f777086
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-10-21 14:25:51 +0330

    #17
    Faucet input has been changed to select.

--- context/handleData.js
+++ context/handleData.js
@@ -398,16 +398,23 @@ export const HandleDataFunctions = (props) => {
 
   //Function for getting the user address for charging money
   const sendMoneyToAddress = () => {
-    const modalElement = (
-      <div>
-        <form className="GiveTokenForm" onSubmit={handleSendMoney}>
-          <label>Please input your wallet address</label>
-          <input id="modalAddressInput" type="text" />
-          <input type="submit" value="submit" />
-        </form>
-      </div>
-    );
-    setModal(modalElement);
+    blockChainFuncs.getWalletLists().then((res) => {
+      const walletList = res.map((item) => (
+        <option key={item.address} value={item.address}>
+          {item.meta.name} : {item.address}
+        </option>
+      ));
+      const modalElement = (
+        <div>
+          <form className="GiveTokenForm" onSubmit={handleSendMoney}>
+            <label>Please Select your wallet address</label>
+            <select id="modalAddressInput">{walletList}</select>
+            <input type="submit" value="submit" />
+          </form>
+        </div>
+      );
+      setModal(modalElement);
+    });
 
     async function handleSendMoney() {
       setModal(null);
