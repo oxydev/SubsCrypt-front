766f8a7e6e87faf2f0fd3b4fbd076aab48aa515f
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-10-25 02:24:33 +0330

    #17
    Tutorial cookie check has been fixed.

--- componenets/gadjets/toggle.js
+++ componenets/gadjets/toggle.js
@@ -1,74 +1,84 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Cookies from "js-cookie";
 
-const CheckedIcon = () => <>📚</>
-const UncheckedIcon = () => <>👩‍💻</>
+const CheckedIcon = () => <>📚</>;
+const UncheckedIcon = () => <>👩‍💻</>;
 
 const ToggleButton = (props) => {
-
-  const [toggle, setToggle] = useState(true)
-  const { defaultChecked, onChange, disabled, className } = props
+  const [toggle, setToggle] = useState(true);
+  const { defaultChecked, onChange, disabled, className } = props;
 
   useEffect(() => {
     if (defaultChecked) {
-      setToggle(defaultChecked)
+      setToggle(defaultChecked);
     }
-  }, [defaultChecked])
+  }, [defaultChecked]);
+
+  useEffect(() => {
+    if (Cookies.get("tutorial") == "off") {
+      setToggle(false);
+    }
+  }, []);
 
   const triggerToggle = () => {
     if (disabled) {
-      return
+      return;
     }
 
-    setToggle(!toggle)
-
-    if (typeof onChange === 'function') {
-      onChange(!toggle)
+    if (toggle) {
+      Cookies.set("tutorial", "off");
+    } else {
+      Cookies.set("tutorial", "on");
     }
-  }
+
+    setToggle(!toggle);
+
+    if (typeof onChange === "function") {
+      onChange(!toggle);
+    }
+  };
 
   const getIcon = (type) => {
-    const { icons } = props
+    const { icons } = props;
     if (!icons) {
-      return null
+      return null;
     }
 
-    return icons[type] === undefined ?
-      ToggleButton.defaultProps.icons[type] :
-      icons[type]
-  }
+    return icons[type] === undefined ? ToggleButton.defaultProps.icons[type] : icons[type];
+  };
 
-  const toggleClasses = classNames('wrg-toggle', {
-    'wrg-toggle--checked': toggle,
-    'wrg-toggle--disabled': disabled
-  }, className)
+  const toggleClasses = classNames(
+    "wrg-toggle",
+    {
+      "wrg-toggle--checked": toggle,
+      "wrg-toggle--disabled": disabled,
+    },
+    className
+  );
 
   return (
-    <div className="title">Tutorial
+    <div className="title">
+      Tutorial
       <div onClick={triggerToggle} className={toggleClasses}>
-          <div className="wrg-toggle-container">
-          <div className="wrg-toggle-check">
-            <span>{getIcon('checked')}</span>
-          </div>
-          <div className="wrg-toggle-uncheck">
-            <span>{getIcon('unchecked')}</span>
-          </div>
+        <div className="wrg-toggle-container">
+          <div className="wrg-toggle-check">{/* <span>{getIcon('checked')}</span> */}</div>
+          <div className="wrg-toggle-uncheck">{/* <span>{getIcon('unchecked')}</span> */}</div>
         </div>
         <div className="wrg-toggle-circle"></div>
-        <input type="checkbox" aria-label="Toggle Button" className="wrg-toggle-input"/>
+        <input type="checkbox" aria-label="Toggle Button" className="wrg-toggle-input" />
       </div>
     </div>
-
-  )
-}
+  );
+};
 
 ToggleButton.defaultProps = {
   icons: {
-    checked: <CheckedIcon/>,
-    unchecked: <UncheckedIcon/>
-  }
-}
+    checked: <CheckedIcon />,
+    unchecked: <UncheckedIcon />,
+  },
+};
 
 ToggleButton.propTypes = {
   disabled: PropTypes.bool,
@@ -79,9 +89,9 @@ ToggleButton.propTypes = {
     PropTypes.bool,
     PropTypes.shape({
       checked: PropTypes.node,
-      unchecked: PropTypes.node
-    })
-  ])
-}
+      unchecked: PropTypes.node,
+    }),
+  ]),
+};
 
-export default ToggleButton
+export default ToggleButton;
--- context/tutorial.js
+++ context/tutorial.js
@@ -1,3 +1,4 @@
+import Cookies from "js-cookie";
 import React, { useEffect, useState } from "react";
 
 export const tutorialContext = React.createContext();
@@ -8,38 +9,34 @@ export const Tutorial = (props) => {
   const [order, setOrder] = useState(0);
   const [position, setPosition] = useState({ top: -400, left: -400 });
 
-  function sleep (ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
   const continueTutorial = async (tutorialData, newOrder) => {
-    console.log(tutorialData)
-    if(order === tutorialData.length - 1)
-      return
-    setOrder(newOrder)
-    let tutList = []
+    console.log(tutorialData);
+    if (order === tutorialData.length - 1) return;
+    setOrder(newOrder);
+    let tutList = [];
     await sleep(500);
     for (const item of tutorialData) {
-      console.log("fuck",document.getElementById(item.elementName),item)
-      while (document.getElementById(item.elementName) == null)
-        await sleep(200)
+      console.log("fuck", document.getElementById(item.elementName), item);
+      while (document.getElementById(item.elementName) == null) await sleep(200);
       tutList.push({
         target: document.getElementById(item.elementName),
         tutorialElement: { title: item.title, description: item.description },
       });
     }
     setTutorialList(tutList);
-
-  }
+  };
 
   const handleTutorial = async (tutorialData) => {
     let tutList = [];
-    setOrder(0)
+    setOrder(0);
     await sleep(500);
 
     for (const item of tutorialData) {
-      while (document.getElementById(item.elementName) == null)
-        await sleep(200)
-      console.log(item,document.getElementById(item.elementName))
+      while (document.getElementById(item.elementName) == null) await sleep(200);
+      console.log(item, document.getElementById(item.elementName));
 
       tutList.push({
         target: document.getElementById(item.elementName),
@@ -50,7 +47,10 @@ export const Tutorial = (props) => {
   };
 
   useEffect(async () => {
-    if (tutorialList && tutorialList.length > 0) {
+    if (Cookies.get("tutorial") == "off") {
+      console.log("hamid");
+      setTutorialList(null);
+    } else if (tutorialList && tutorialList.length > 0) {
       for (const elm of tutorialList) {
         elm.target.classList.remove("Dominent");
       }
@@ -67,10 +67,9 @@ export const Tutorial = (props) => {
     }
   }, [order, tutorialList]);
 
-
   const checkPos = async (elm) => {
-    elm.scrollIntoView({block:'center'});
-    await sleep(100)
+    elm.scrollIntoView({ block: "center" });
+    await sleep(100);
 
     let pos = elm.getBoundingClientRect();
     let vw = window.innerWidth;
@@ -81,7 +80,6 @@ export const Tutorial = (props) => {
     let bottom = vh - pos.bottom;
     let height = pos.height;
     let width = pos.width;
-
 
     let place = { vertical: "middle", horizontal: "right" };
     if (top > 160 && bottom > 160) {
@@ -116,7 +114,7 @@ export const Tutorial = (props) => {
   };
 
   return (
-    <tutorialContext.Provider value={{ handleTutorial, continueTutorial}}>
+    <tutorialContext.Provider value={{ handleTutorial, continueTutorial }}>
       <>
         {props.children}
         {tutorialList && tutorialList.length > 0 && (
