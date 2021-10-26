8dc1932158d62b785ea2ece40b482cbc6d4497ce
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-10-26 11:38:39 +0330

    #17
    New header design has been added.

--- componenets/layOut/header.js
+++ componenets/layOut/header.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 //The component for generating page header
 export default function Header() {
   const { globalState } = useContext(UserContext);
-  const { handleLogOut } = useContext(handleDataContext);
+  // const { handleLogOut } = useContext(handleDataContext);
   const [showMenu, setShownMenu] = useState(false);
 
   const userName = globalState.user.username;
@@ -34,7 +34,25 @@ export default function Header() {
         <div className="UserMenu-button"></div>
         <div className={showMenu ? "UserMenu-menu" : "UserMenu-menu hidden"}>
           <ul>
-            {globalState.user.username && (
+            {userName && (
+              <li>
+                <p>
+                  <embed src={"/icons/png/sideBar/user.svg"} />
+                  {userName}
+                </p>
+              </li>
+            )}
+
+            {userWallet && (
+              <li>
+                <p>
+                  <embed src={"/icons/png/sideBar/wallet.svg"} />
+                  {middleDots(userWallet)}
+                </p>
+              </li>
+            )}
+
+            {/* {globalState.user.username && (
               <li>
                 <Link
                   href={
@@ -58,7 +76,7 @@ export default function Header() {
                   LogOut
                 </a>
               </Link>
-            </li>
+            </li> */}
           </ul>
         </div>
       </div>
