5b3e29a1fa9c9454266fd5d2a61e57dca2918ffd
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-10-26 12:15:46 +0330

    #17
    User menu and identity menu has been updated in header.

--- componenets/layOut/header.js
+++ componenets/layOut/header.js
@@ -2,13 +2,14 @@ import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/store";
 import { handleDataContext } from "../../context/handleData";
 import { middleDots } from "../../utilities/utilityFunctions";
-// import Link from "next/link";
+import Link from "next/link";
 
 //The component for generating page header
 export default function Header() {
   const { globalState } = useContext(UserContext);
-  const { handleWalletBalance } = useContext(handleDataContext);
+  const { handleWalletBalance, handleLogOut } = useContext(handleDataContext);
   const [showMenu, setShownMenu] = useState(false);
+  const [showIdentity, setShowIdentity] = useState(false);
 
   const userName = globalState.user.username;
   const userWallet = globalState.user.address;
@@ -21,24 +22,23 @@ export default function Header() {
 
   return (
     <div className="Header">
-      <div className={userName || userWallet ? "LoginBox" : "LoginBox Green"}>
+      <div
+        className={userName || userWallet ? "LoginBox" : "LoginBox Green"}
+        onMouseOver={() => {
+          setShowIdentity(true);
+        }}
+        onMouseOut={() => {
+          setShowIdentity(false);
+        }}
+      >
         <img className="Avatar" src={"/avatar/notLogedin.svg"} />
         {/* show each of user name or wallet address or both of them */}
         {userName && <p className="UserName">{userName}</p>}
         {userName && userWallet && <p className="Separator">|</p>}
         {userWallet && <p className="UserWallet">{middleDots(userWallet)}</p>}
-      </div>
-      <div
-        className="UserMenu"
-        onMouseOver={() => {
-          setShownMenu(true);
-        }}
-        onMouseOut={() => {
-          setShownMenu(false);
-        }}
-      >
-        <div className="UserMenu-button"></div>
-        <div className={showMenu ? "UserMenu-menu" : "UserMenu-menu hidden"}>
+        <div
+          className={showIdentity ? "UserMenu-menu" : "UserMenu-menu hidden"}
+        >
           <ul>
             {userName && (
               <li>
@@ -90,6 +90,46 @@ export default function Header() {
                 </a>
               </Link>
             </li> */}
+          </ul>
+        </div>
+      </div>
+      <div
+        className="UserMenu"
+        onMouseOver={() => {
+          setShownMenu(true);
+        }}
+        onMouseOut={() => {
+          setShownMenu(false);
+        }}
+      >
+        <div className="UserMenu-button"></div>
+        <div className={showMenu ? "UserMenu-menu" : "UserMenu-menu hidden"}>
+          <ul>
+            {globalState.user.username && (
+              <li>
+                <Link
+                  href={
+                    globalState.user.type === "user"
+                      ? "/user/profilesetting"
+                      : "/provider/profilesetting"
+                  }
+                >
+                  <a>
+                    {" "}
+                    <embed src={"/icons/png/sideBar/setting-black.svg"} />
+                    Profile Setting
+                  </a>
+                </Link>
+              </li>
+            )}
+            <li>
+              <Link href="#">
+                <a onClick={handleLogOut}>
+                  <embed src={"/icons/png/sideBar/logout-black.svg"} />
+                  LogOut
+                </a>
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
