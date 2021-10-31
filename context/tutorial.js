adcb59131f7470c399c77cea7e53cd444beb8245
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-11-01 02:34:10 +0330

    Tutorial style set to be like the design

--- context/tutorial.js
+++ context/tutorial.js
@@ -9,7 +9,7 @@ export const Tutorial = (props) => {
   const [run, setRun] = useState(false);
 
   const handleTutorial = (tutLists) => {
-    // setSteps([...tutLists]);
+    setSteps([...tutLists]);
   };
 
   const continueTutorial = (useless) => {
@@ -24,127 +24,40 @@ export const Tutorial = (props) => {
     // console.log(steps, run);
   }, [steps, run]);
 
+  const tooltip = ({
+    continuous,
+    index,
+    step,
+    backProps,
+    closeProps,
+    primaryProps,
+    tooltipProps,
+  }) => (
+    <div className="TutorialBox" {...tooltipProps}>
+      <img src="/logo/tutLogo.png" />
+      <h2>{step.title}</h2>
+      <p>{step.content}</p>
+      <div className="BtnContainer">
+        {index > 0 && <button {...backProps}>Previous</button>}
+        {continuous && <button {...primaryProps}>Next</button>}
+        {!continuous && <button {...closeProps}>close</button>}
+      </div>
+    </div>
+  );
+
   return (
     <tutorialContext.Provider value={{ handleTutorial, continueTutorial }}>
       <>
-        {steps && <ReactJoyride steps={steps} continuous={true} run={run} />}
+        {steps && (
+          <ReactJoyride
+            steps={steps}
+            continuous={true}
+            run={run}
+            tooltipComponent={tooltip}
+          />
+        )}
         {props.children}
       </>
     </tutorialContext.Provider>
   );
 };
-
-// const [tutorialList, setTutorialList] = useState(null);
-//   //lists is like : [{target: dom ref, tutorialElement: jsx}]
-//   const [order, setOrder] = useState(0);
-//   const [position, setPosition] = useState({ top: -400, left: -400 });
-
-//   function sleep(ms) {
-//     return new Promise((resolve) => setTimeout(resolve, ms));
-//   }
-//   const continueTutorial = async (tutorialData, newOrder) => {
-//     // console.log(tutorialData);
-//     if (order === tutorialData.length - 1) return;
-//     setOrder(newOrder);
-//     let tutList = [];
-//     await sleep(500);
-//     for (const item of tutorialData) {
-//       // console.log("fuck", document.getElementById(item.elementName), item);
-//       while (document.getElementById(item.elementName) == null)
-//         await sleep(200);
-//       tutList.push({
-//         target: document.getElementById(item.elementName),
-//         tutorialElement: { title: item.title, description: item.description },
-//       });
-//     }
-//     setTutorialList(tutList);
-//   };
-
-//   const handleTutorial = async (tutorialData) => {
-//     let tutList = [];
-//     setOrder(0);
-//     await sleep(500);
-
-//     for (const item of tutorialData) {
-//       // console.log(item, document.getElementById(item.elementName));
-
-//       while (document.getElementById(item.elementName) == null)
-//         await sleep(200);
-
-//       tutList.push({
-//         target: document.getElementById(item.elementName),
-//         tutorialElement: { title: item.title, description: item.description },
-//       });
-//     }
-//     setTutorialList(tutList);
-//   };
-
-//   useEffect(async () => {
-//     if (Cookies.get("tutorial") === "off") {
-//       setTutorialList(null);
-//     } else if (tutorialList && tutorialList.length > 0) {
-//       for (const elm of tutorialList) {
-//         elm.target.classList.remove("Dominent");
-//       }
-//       const target = tutorialList[order].target;
-//       const tutPos = await checkPos(target);
-//       setPosition({
-//         top: tutPos.top,
-//         left: tutPos.left,
-//         vertical: tutPos.vertical,
-//         horizontal: tutPos.horizontal,
-//       });
-//       target.classList.add("Dominent");
-//       // console.log(position);
-//     }
-//   }, [order, tutorialList]);
-
-//   const checkPos = async (elm) => {
-//     elm.scrollIntoView({ block: "center" });
-//     await sleep(100);
-
-//     let pos = elm.getBoundingClientRect();
-//     let vw = window.innerWidth;
-//     let vh = window.innerHeight;
-//     let top = pos.top;
-//     let left = pos.left;
-//     let right = vw - pos.right;
-//     let bottom = vh - pos.bottom;
-//     let height = pos.height;
-//     let width = pos.width;
-
-//     let place = { vertical: "middle", horizontal: "right" };
-//     if (top > 160 && bottom > 160) {
-//       place.vertical = "middle";
-//       place.top = top + height / 2 - 155;
-//     } else if (top < 160) {
-//       place.vertical = "bottom";
-//       place.top = top + height + 100;
-//     } else {
-//       place.vertical = "top";
-//       place.top = top - 409;
-//     }
-//     if (right > 400) {
-//       place.horizontal = "right";
-//       place.left = left + width + 100;
-//     } else if (left > 400) {
-//       place.horizontal = "left";
-//       place.left = left - 409;
-//     } else {
-//       place.horizontal = "middle";
-//       place.left = left + width / 2 - 155;
-//     }
-
-//     if (place.horizontal == "middle" && place.vertical == "middle") {
-//       place.vertical = "bottom";
-//       place.top = top + height + 100;
-//     }
-//     return place;
-//   };
-
-//   const endTutorial = () => {
-//     for (const elm of tutorialList) {
-//       elm.target.classList.remove("Dominent");
-//     }
-//     setTutorialList(null);
-//   };
