38d0d9cde4553e8cb6a5ae432ecd50c2ec2c5788
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2021-11-01 03:04:26 +0330

    Tutorial restarting problem has been fixed.

--- context/tutorial.js
+++ context/tutorial.js
@@ -1,12 +1,12 @@
 import Cookies from "js-cookie";
 import React, { useEffect, useState } from "react";
-import ReactJoyride from "react-joyride";
-
+import ReactJoyride, { ACTIONS, EVENTS, STATUS } from "react-joyride";
 export const tutorialContext = React.createContext();
 
 export const Tutorial = (props) => {
   const [steps, setSteps] = useState(null);
-  const [run, setRun] = useState(false);
+  const [run, setRun] = useState(true);
+  const [state, setState] = useState({ run: false, index: 0 });
 
   const handleTutorial = (tutLists) => {
     setSteps([...tutLists]);
@@ -21,7 +21,7 @@ export const Tutorial = (props) => {
   }, [steps]);
 
   useEffect(() => {
-    // console.log(steps, run);
+    setState({ run: true, index: 0 });
   }, [steps, run]);
 
   const tooltip = ({
@@ -45,15 +45,32 @@ export const Tutorial = (props) => {
     </div>
   );
 
+  const handleJoyrideCallback = (data) => {
+    const { action, index, status, type } = data;
+
+    if ([EVENTS.STEP_AFTER, EVENTS.TARGET_NOT_FOUND].includes(type)) {
+      // Update state to advance the tour
+      setState({ index: index + (action === ACTIONS.PREV ? -1 : 1) });
+    } else if ([STATUS.FINISHED, STATUS.SKIPPED].includes(status)) {
+      // Need to set our running state to false, so we can restart if we click start again.
+      setState({ run: false });
+    }
+    console.groupCollapsed(type);
+    console.log(data); //eslint-disable-line no-console
+    console.groupEnd();
+  };
+
   return (
     <tutorialContext.Provider value={{ handleTutorial, continueTutorial }}>
       <>
         {steps && (
           <ReactJoyride
+            callback={handleJoyrideCallback}
             steps={steps}
             continuous={true}
-            run={run}
+            run={state.run}
             tooltipComponent={tooltip}
+            index={state.index}
           />
         )}
         {props.children}
